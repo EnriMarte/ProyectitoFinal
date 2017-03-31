2d202198dc67797f78180d1261b67e2b18c8698e
ENRICO GUILLERMO MARTELLA <42341041@ort>
2017-03-31 12:09:26 -0300

    Empezando con la primera view.xml

--- ProyectitoFinal/app/src/main/java/com/example/a42341041/proyectitofinal/login.java
+++ ProyectitoFinal/app/src/main/java/com/example/a42341041/proyectitofinal/login.java
@@ -2,12 +2,14 @@ package com.example.a42341041.proyectitofinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
     }
 }
