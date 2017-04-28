62f52c042c498185999b90f00117b3cfda20aa17
IAN FRANCO ROSSI <42567321@ort>
2017-04-28 11:58:34 -0300

    Subo archivo android vacio

---
+++ ProyectoFinal_ANDROID/app/src/main/java/com/example/a42567321/proyectofinal_android/principal.java
@@ -0,0 +1,13 @@
+package com.example.a42567321.proyectofinal_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
