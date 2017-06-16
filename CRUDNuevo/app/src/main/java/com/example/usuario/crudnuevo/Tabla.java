db41167c586139cd4071c2f1025f04fa38fab8ac
Diego Pertierra <dpertierra@hotmail.com>
2017-06-16 19:23:37 -0300

    CRUD Nuevo

---
+++ CRUDNuevo/app/src/main/java/com/example/usuario/crudnuevo/Tabla.java
@@ -0,0 +1,26 @@
+package com.example.usuario.crudnuevo;
+
+/**
+ * Created by Usuario on 16/6/2017.
+ */
+
+public class Tabla {
+    String Texto;
+    private int Id;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getTexto() {
+        return Texto;
+    }
+
+    public void setTexto(String texto) {
+        Texto = texto;
+    }
+}
